ce42f523fbd49d456c216405a125bf7b7c52ae98
Meha <ahem4progs@gmail.com>
2020-04-20 15:00:57 +0530

    Rendered UI of clock

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import ReactDom from 'react-dom';
+import ReactDOM from 'react-dom';
 
 function tick() {
-  const Element = (
+  const element = (
     <div>
       <h2>Hello World</h2>
       <p> Current Time is: {new Date().toLocaleTimeString()}.</p>
     </div>
   );
-  
+  ReactDOM.render(
+    element,
+    document.getElementById('root')
+  );
 }
+setInterval(tick(), 1000);
